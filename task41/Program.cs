495854d9e4510079306655416426b77bfcce31a7
Roman <user@email.com>
2023-07-27 02:27:05 +0500

    added homework for seminar 6

---
+++ task41/Program.cs
@@ -0,0 +1,39 @@
+﻿int Zapros(string message) {
+    Console.WriteLine(message);
+    int num = System.Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int count = Zapros("Сколько чисел вы хотите ввести?");
+
+int[] FillArr(int Count) {
+   int[] arr = new int[count];
+   for (int i = 0; i < arr.Length; i++)
+   {
+        Console.Write($"Введите {i + 1} число = ");
+        arr[i] = System.Convert.ToInt32(Console.ReadLine());
+   }
+   return arr;
+}
+
+void WhichBigger(int[] arr) {
+    int max = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(max < arr[i]) {
+            max = arr[i];
+        }
+    }
+    Console.WriteLine($"Самое большое введенное число это - {max}");
+}
+
+int[] Mass = FillArr(count);
+Console.Write(string.Join("|", Mass));
+Console.WriteLine();
+WhichBigger(Mass);
+
+
+
+
+
+
---
+++ task43/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int Zapros(string message) {
+    Console.WriteLine(message);
+    int num = System.Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+double b1 = Zapros("Введите b1");
+double k1 = Zapros("Введите k1");
+double b2 = Zapros("Введите b2");
+double k2 = Zapros("Введите k2");
+
+void FindDot(double B1, double K1, double B2, double K2) {
+    double x = (B1 - B2) / (K2 - K1);
+    double y = K1 * x + B1;
+    Console.WriteLine($"x = {x}");
+    Console.WriteLine($"y = {y}");
+}
+
+FindDot(b1,k1,b2,k2);
+
